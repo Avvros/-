16b4bda1580389b39650dd126774f0e433542567
Александр Жариков <alexzharikov34@gmail.com>
2023-09-17 19:54:44 +0300

    some improvements

--- src/approx.py
+++ src/approx.py
@@ -1,35 +1,33 @@
 import numpy as np
 
 
-def make_eq_terms(x: list, idx: int, m: int):
-    for t in range(len(x)):
+def make_eq_terms(x: list, idx: int, n: int, m: int):
+    for t in range(n):
         der = x[t] ** idx
         yield [x[t] ** index * der for index in range(m + 1)]
 
 
 def polyfill(x: list, y: list, m: int):
-    eq_system = [[sum(coeff_terms) for coeff_terms in zip(*make_eq_terms(x, i, m))] for i in range(m + 1)]
+    n = min(len(x), m + 1)
+    eq_system = [[sum(coeff_terms) for coeff_terms in zip(*make_eq_terms(x, i, n, m))] for i in range(m + 1)]
     # for row in eq_system:
     #     print()
     #     print(*row)
     #     print()
     A = np.array(eq_system, dtype=float)
     #print(A)
-    B = np.fromiter((sum(y[t] * x[t] ** i for t in range(len(x))) for i in range(m + 1)), dtype=float)
+    B = np.fromiter((sum(y[t] * x[t] ** i for t in range(n)) for i in range(m + 1)), dtype=float)
     #B = np.array((sum(y[t] * x[t] ** i for t in range(len(x))) for i in range(m + 1)), dtype=float)
     #print(B)
     R = np.linalg.solve(A, B)
-    with np.printoptions(precision=3, suppress=True):
-        print(R)
+    #with np.printoptions(precision=5, suppress=True):
+        #print(R)
+    return R
 
 
-
-
-
-#x = [0, 1, 2]
-#y = [0, 1, 4]
-#polyfill(x, y, 2)
-
-
-#print()
-#load("vals.csv")
+if __name__ == "__main__":
+    #x = [0, 1, 2]
+    #y = [0, 1, 4]
+    x = [1, 2, 3]
+    y = [0.54, -0.416, -0.989]
+    polyfill(x, y, 2)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
+import numpy as np
 import locale
 import csv
+import enum
+import approx
 
 locale.setlocale(locale.LC_ALL, '')
+
+
+class StandardAtmosphereParameters(enum.Enum):
+    geom_height = 0
+    geop_height = 1
+    temperature_K = 2
+    temperature_C = 3
+    pressure_Pa = 4
+    pressure_mmHg = 5
+    density = 6
+    gravity_acceleration = 7
+
 
 def load(path: str):
     with open(path, encoding="utf-8-sig") as csv_file:
@@ -9,12 +24,11 @@ def load(path: str):
         header = next(csv_reader)
         table = list(zip(header, map(lambda column: list(map(locale.atof, column)), zip(*csv_reader))))
         return table
-        # print(" | ".join(header))
-        # for row in csv_reader:
-        #     try:
-        #         print(*map(locale.atof, row))
-        #     except ValueError as ve:
-        #         print(ve)
 
 
-print(load("st_atm.csv"))
+st_atm = load("st_atm.csv")
+base_index = StandardAtmosphereParameters.geom_height.value
+with np.printoptions(precision=5, suppress=True):
+    print(approx.polyfill(st_atm[base_index][1], st_atm[StandardAtmosphereParameters.density.value][1], 2))
+#print(st_atm[base_index][1])
+#print(st_atm[StandardAtmosphereParameters.density.value][1])
